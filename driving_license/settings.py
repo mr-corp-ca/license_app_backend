caec240ca76a347a2fe345320634a12a445e9181
faizanmehdi-pro <faizanmehdi69@gmail.com>
2024-12-16 19:19:26 +0500

    https origin in trusted origin

--- driving_license/settings.py
+++ driving_license/settings.py
@@ -51,6 +51,7 @@ CORS_ORIGIN_WHITELIST = [
 CSRF_TRUSTED_ORIGINS = [
     'http://192.168.10.40:9000/',
     'http://api.thegearup.ca/',
+    'https://api.thegearup.ca/',
     'http://192.168.10.95:9000/'
 
 ]
