cce44d9a4e72f9c4eb399773ba552000773e2a97
faizanmehdi-pro <faizanmehdi69@gmail.com>
2024-12-29 00:08:21 +0500

    logs model

--- course_management_app/views.py
+++ course_management_app/views.py
@@ -42,7 +42,7 @@ class CourseApiView(APIView):
         except:
             pass
         
-        LogsModel.objects.create(json_data=f'Type..........{type(lessons)}.............data...........{lessons}')
+        LogsModel.objects.create(json_data=f'Type......................data...........{request.data}')
 
         lesson_count = len(lessons)
         if lesson_numbers != lesson_count:
